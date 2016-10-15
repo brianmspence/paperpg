f01c9515503119ab7a03a7e4af7ef17130638598
Brian Spence <brianmspence@gmail.com>
2016-10-15 10:35:41 -0700

    Added first start on form

--- public/scripts/gurps.js
+++ public/scripts/gurps.js
@@ -1,3 +1,15 @@
+var makeSkill = function() {
+	return {
+		name:'',
+		attribute:'',
+		difficultry:'',
+		defaults:[],
+		page:0,
+		specialization:false,
+		description:''
+	};
+};
+
 var SkillEntry = React.createClass({
     
     getInitialState: function() {
@@ -7,6 +19,15 @@ var SkillEntry = React.createClass({
         return (
             <div >
                 <h2>Skill Entry</h2>
+		<form>
+		<div>Name:<input type="text" name="name"/></div>
+		<div>Attribute:<input type="text" name="attribute"/></div>
+		<div>Difficulty:<input type="text" name="difficulty"/></div>
+		<div>Defaults:<input type="text" name="defaults"/></div>
+		<div>Page:<input type="text" name="page"/></div>
+		<div>Requires Specialization:<input type="checkbox" name="specialization"/></div>
+		<div>Description:<textarea/></div>
+		</form>
             </div>
             );
   }
