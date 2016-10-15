a9de18129ef6bfeed304b5f85157906854779904
Brian Spence <brianmspence@gmail.com>
2016-10-15 09:43:30 -0700

    Added simple React page

---
+++ public/scripts/gurps.js
@@ -0,0 +1,18 @@
+var SkillEntry = React.createClass({
+    
+    getInitialState: function() {
+        return {};
+    },
+    render: function() {
+        return (
+            <div >
+                <h2>Skill Entry</h2>
+            </div>
+            );
+  }
+});
+
+ReactDOM.render(
+  <SkillEntry />,
+  document.getElementById('container')
+);
