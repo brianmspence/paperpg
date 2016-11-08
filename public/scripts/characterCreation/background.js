05aaf38dcbecfab14428b832a92cc032c50dcbe5
Brian Spence <brianmspence@gmail.com>
2016-11-07 17:48:53 -0800

    Improved Languages table

--- public/scripts/characterCreation/background.js
+++ public/scripts/characterCreation/background.js
@@ -11,9 +11,53 @@ var Background = React.createClass({
             Status:0,
             Rep:0,
             Wealth:0,
-            Rank:0
+            Rank:0,
+            langName:'',
+            spoken:'Native',
+            written:'Native',
+            languages:[
+              {
+                name:'Common',
+                spoken:'Native',
+                written:'Native'
+              },
+              {
+                name:'French',
+                spoken:'Broken',
+                written:'None'
+              }
+            ]
         };
         
+    },
+    calcLangCost: function(lang, index) {
+      var getCost = function(level) {
+        switch(level) {
+          case 'Native':
+            return 3;
+            break;
+          case 'Accented':
+            return 2;
+            break;
+          case 'Broken':
+            return 1;
+            break;
+          case 'None':
+            return 0;
+            break;
+          default:
+            return 0;
+        }
+        return 0;
+      };
+      var spokenCost = getCost(lang.spoken);
+      var writtenCost = getCost(lang.written);
+      var cost = spokenCost + writtenCost;
+      if(index === 0) {
+        cost = cost - 6;
+      }
+
+      return cost;
     },
     handleTLChange: function(e) {
     	this.setState({TL: e.target.value});
@@ -30,6 +74,39 @@ var Background = React.createClass({
     handleRankChange: function(e) {
         this.setState({Rank: e.target.value});
     },
+    handleLangNameChange: function(e) {
+      this.setState({langName:e.target.value});
+    },
+    handleWrittenChange: function(e) {
+      this.setState({written:e.target.value});
+    },
+    handleSpokenChange :function(e) {
+      this.setState({spoken:e.target.value});
+    },
+    handleLangAddClick: function(e) {
+      var name = this.state.langName;
+      var written = this.state.written;
+      var spoken = this.state.spoken;
+
+      if(name === '') {
+        return;
+      }
+
+      var newLanguage = {
+        name:name,
+        spoken:spoken,
+        written:written
+      };
+
+      var data = [...this.state.languages, newLanguage];
+
+      this.setState({languages:data, langName:'', spoken:'Native', written:'Native'});
+    },
+    handleLangRemoveClick: function(e, index) {
+        var data = [...this.state.languages];
+        var removedItem = data.splice(index, 1);
+        this.setState({languages:data});
+    },
     render: function() {
         return (
 			<div>
@@ -42,27 +119,74 @@ var Background = React.createClass({
                 <div>
                     <h3>Languages</h3>
                     <Table
-                        rowsCount={1}
+                        rowsCount={this.state.languages.length}
                         rowHeight={30}
-                        width={440}
-                        height={60}
-                        headerHeight={30}>
+                        width={514}
+                        height={this.state.languages.length*30 + 30 + 30 + 2}
+                        headerHeight={30}
+                        footerHeight={30}>
                         <Column
                             header={<Cell>Language</Cell>}
-                          cell={<Cell>Common</Cell>}
+                          cell={props => (
+                            <Cell {...props}>
+                              {this.state.languages[props.rowIndex].name}
+                            </Cell>
+                          )}
+                          footer={<Cell>
+                                    <input
+                                        type="text"
+                                        value={this.state.langName}
+                                        onChange={(e) => this.handleLangNameChange(e)} />
+                                </Cell>}
                           width={200} />
                           <Column
                             header={<Cell>Spoken</Cell>}
-                          cell={<Cell>Native</Cell>}
+                            cell={props => (
+                              <Cell {...props}>
+                                {this.state.languages[props.rowIndex].spoken}
+                              </Cell>
+                            )}
+                            footer={<Cell>
+                                      <select value={this.state.spoken} onChange={(e) => this.handleSpokenChange(e)}>
+                                        <option value="None">None</option>
+                                        <option value="Broken">Broken</option>
+                                        <option value="Accented">Accented</option>
+                                        <option value="Native">Native</option>
+                                      </select>
+                                    </Cell>}
                           width={80} />
                           <Column
                             header={<Cell>Written</Cell>}
-                          cell={<Cell>Literacy</Cell>}
+                            cell={props => (
+                              <Cell {...props}>
+                                {this.state.languages[props.rowIndex].written}
+                              </Cell>
+                            )}
+                            footer={<Cell>
+                                      <select value={this.state.written} onChange={(e) => this.handleWrittenChange(e)}>
+                                        <option value="None">None</option>
+                                        <option value="Broken">Broken</option>
+                                        <option value="Accented">Accented</option>
+                                        <option value="Native">Native</option>
+                                      </select>
+                                    </Cell>}
                           width={80} />
                           <Column
                             header={<Cell>Cost</Cell>}
-                          cell={<Cell>0</Cell>}
+                          cell={props => (
+                              <Cell {...props}>
+                                {this.calcLangCost(this.state.languages[props.rowIndex], props.rowIndex)}
+                              </Cell>
+                            )}
                           width={80} />
+                          <Column
+                            cell={props => (
+                              <Cell><button onClick={(e) => this.handleLangRemoveClick(e, props.rowIndex)}>Remove</button></Cell>
+                            )}
+                            footer={<Cell>
+                              <button onClick={(e) => this.handleLangAddClick(e)}>Add</button>
+                            </Cell>}
+                            width={74} />                          
                     </Table>
                 </div>
                 <div>
